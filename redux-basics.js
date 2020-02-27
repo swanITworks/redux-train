316dcb76daaf909a0390b4105ad71d9ed264b0b4
swanITworksl <michal@labi.pl>
2020-02-27 21:55:57 +0100

    added redux store and conection with counter component

--- redux-basics.js
+++ redux-basics.js
@@ -1,4 +1,3 @@
-
 const redux = require('redux');
 const createStore = redux.createStore;
 
@@ -8,6 +7,19 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
@@ -15,8 +27,14 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-
+// Subscription
+store.subscribe(() => {
+        console.log('[SUBSCRIPTION]',store.getState());
+    }
+);
 
 // Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
-// Subscription
